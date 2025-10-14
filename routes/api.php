98e19e10f25e1c37dd53497e07b2c7d936b16834
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-13 23:52:55 -0700

    Reinstalled React jsx ahead typescript

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,1 @@
-<?php
 
-
-use App\Http\Controllers\UserController\AuthController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
