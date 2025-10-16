a7064042c50e8bbd82455f6eb923f8c7966229a6
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-15 21:43:40 -0700

    Changed position of navbar

--- resources/frontend/src/Components/HomeComponent/Home.jsx
+++ resources/frontend/src/Components/HomeComponent/Home.jsx
@@ -2,10 +2,9 @@ import SliderSection from "./SliderSection";
 import TopProduct from "./TopProduct.jsx";
 import CategoriesMarquee from "./CategoriesMarquee.jsx";
 
-
 function Home() {
     return (
-        <div className="h-auto bg-gray-50 pt-10 pb-20">
+        <div className="h-auto bg-gray-50 pt-32 pb-20">
             <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-6 px-0">
                 <div className="w-full lg:w-4/6 lg:h-[500px]">
                     <SliderSection />
@@ -13,7 +12,6 @@ function Home() {
                 <TopProduct/>
             </div>
             <CategoriesMarquee/>
-
         </div>
     );
 }
