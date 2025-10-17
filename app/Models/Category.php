804fae315fe7c902d4986e0d6c90bc32ec4d2acf
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-16 23:14:18 -0700

    Add new Import files for product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,8 @@ class Category extends Model
 
     protected $fillable = ['name', 'description'];
 
-
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
 
---
+++ database/migrations/2025_10_17_060337_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('item_code')->unique();
+            $table->string('name');
+            $table->string('model')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->nullable();
+            $table->integer('availability')->default(0);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
