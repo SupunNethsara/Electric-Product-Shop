002aaeaae1b70d4d6f4c2059dd0b7e5a4751f54b
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-22 07:51:25 -0700

    Change structure of routes

---
+++ routes/modules/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\UserController\AuthController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
---
+++ routes/modules/products.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProductController\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/active', [ProductController::class, 'getActiveProducts']);
+Route::get('/products/home', [ProductController::class, 'homeProducts']);
+Route::post('/products/validate', [ProductController::class, 'validateFiles']);
+Route::post('/products/upload', [ProductController::class, 'uploadProducts']);
+Route::post('/products/upload-images', [ProductController::class, 'uploadImages']);
