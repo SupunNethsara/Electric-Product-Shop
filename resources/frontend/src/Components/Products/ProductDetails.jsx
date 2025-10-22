948bc84c779121d3b932bcf2a247b3df265820ff
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-22 06:15:44 -0700

    Make product details section

--- resources/frontend/src/Components/Products/ProductDetails.jsx
+++ resources/frontend/src/Components/Products/ProductDetails.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import {useLocation, useParams} from 'react-router-dom';
 
 function ProductDetails() {
-    const { id } = useParams();
-
+    const location = useLocation();
+    const { product } = location.state || {};
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">Product Details</h1>
-            <p>Product ID: {id}</p>
+
         </div>
     );
 }
