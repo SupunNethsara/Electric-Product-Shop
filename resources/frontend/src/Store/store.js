7db705316a8a764887c301e4def02a0908276058
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-29 00:41:01 -0700

    Add cartReducer to redux store

--- resources/frontend/src/Store/store.js
+++ resources/frontend/src/Store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import modalReducer from "./slices/modalSlice";
+import cartReducer from "./slices/cartSlice";
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         modal: modalReducer,
+        cart: cartReducer,
     },
 });
