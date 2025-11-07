6235a47e600e188468e67463a0d69a0a5b3f56f2
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-11-07 01:18:02 -0800

    Fixed issue

--- database/migrations/2025_11_07_091031_create_system_settings_table.php
+++ database/migrations/2025_11_07_091031_create_system_settings_table.php
@@ -4,7 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('system_settings', function (Blueprint $table) {
@@ -13,12 +17,17 @@ return new class extends Migration {
             $table->string('admin_email')->default('admin@store.com');
             $table->string('mobile')->nullable();
             $table->string('address')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('logo_url')->nullable();
             $table->text('site_description')->nullable();
             $table->integer('items_per_page')->default(24);
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('system_settings');
