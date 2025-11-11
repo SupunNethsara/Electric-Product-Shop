6043cf71b66c563ff1bb28118dd6fce17f401547
Supun Nethsara <supunmax663@gmail.com>
2025-11-11 19:49:33 +0530

    Fixed issue

--- app/Http/Controllers/UserController/AuthController.php
+++ app/Http/Controllers/UserController/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
